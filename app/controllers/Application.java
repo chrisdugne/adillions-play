5f19adc4ac220e5cba13440cb5083c684a79e3e4
Chris <chris.dugne@uralys.com>
2013-10-15 11:35:07 +0200

    test OG + conf prod

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,15 +11,32 @@ import play.mvc.Result;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import views.html.*;
+import views.html.mobile.login;
+import views.html.mobile.signin;
+import views.html.mobile.signinFB;
+import views.html.mobile.video;
+import views.html.mobile.invite;
+import views.html.main;
 
 public class Application extends Controller {
 
-	public static final String FLASH_MESSAGE_KEY = "message";
-	public static final String FLASH_ERROR_KEY = "error";
-	public static final String USER_ROLE = "user";
+	// -----------------------------------------------------------------------------------//
+	// facebook
 
-	// ---------------------------------------------//
+	protected static final String FACEBOOK_APP_ID 				= "170148346520274";
+	protected static final String FACEBOOK_API_SECRET 			= "887e8f7abb9b1cb9238a097e06585ae2";
+	protected static final String FACEBOOK_APP_NAMESPACE 		= "adillions";
+
+//	protected static final String APP_HOSTNAME 					= "https://kdo-serendi.herokuapp.com";
+	protected static final String APP_HOSTNAME 					= "http://192.168.0.9:9000";
+
+	// -----------------------------------------------------------------------------------//
+
+	public static final String FLASH_MESSAGE_KEY 				= "message";
+	public static final String FLASH_ERROR_KEY 					= "error";
+	public static final String USER_ROLE 							= "user";
+
+	// -----------------------------------------------------------------------------------//
 
 	protected static Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 	
---
+++ app/controllers/OpenGraph.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import java.util.Map;
+
+import play.mvc.Result;
+
+import views.html.objectpages.theme;
+
+public class OpenGraph extends Application
+{
+
+	// ---------------------------------------------//
+
+	/**
+	 * Facebook dynamic Object Page pour Theme
+	 */
+	public static Result theme()
+	{
+
+		Map<String, String[]> queryParameters = request().queryString();
+
+		String title 			= null;
+		String description 	= null;
+		String imageURL 		= null;
+		String objectPageURL = null;
+		String locale 			= null;
+
+		try
+		{
+			description = queryParameters.get("description")[0];
+			title = queryParameters.get("title")[0];
+			imageURL = queryParameters.get("imageURL")[0];
+			locale = queryParameters.get("locale")[0];
+			objectPageURL = APP_HOSTNAME + request().uri();
+		}
+		catch (Exception e)
+		{
+			if (description == null)
+				description = "";
+			if (title == null)
+				title = "";
+		}
+
+		return ok(theme.render(
+				FACEBOOK_APP_NAMESPACE,
+				FACEBOOK_APP_ID,
+				title,
+				description,
+				imageURL,
+				objectPageURL,
+				locale));
+	}
+
+	// ---------------------------------------------//
+}
