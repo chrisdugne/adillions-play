b024725055c9f4a3b064e48a093ba1b0438e0193
Chris <chris.dugne@uralys.com>
2013-11-15 14:28:55 +0100

    next

--- app/managers/AccountManager.java
+++ app/managers/AccountManager.java
@@ -446,7 +446,8 @@ public class AccountManager {
 				"<p>firstName : " + player.getFirstName() + "</p>" + 
 				"<p>lastName : " + player.getLastName() + "</p>" + 
 				"<p>birthdate : " + player.getBirthDate() + "</p>" + 
-				"<p>amount : " + Utils.roundOneDecimals(amount) + " euros (player's country : "+country+")</p>" ; 
+				"<p>amount : " + Utils.roundOneDecimals(amount) + "</p>" + 
+				"<p>country : " +country+"</p>" ; 
 		
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		mail.setSubject(subject);
