3a834cc17ef43c6176a59a3c2ede5b29af9f1e75
Chris <chris.dugne@uralys.com>
2014-03-06 22:21:03 +0100

    versionRequired

--- app/models/Global.java
+++ app/models/Global.java
@@ -18,21 +18,24 @@ public class Global extends Model {
     @Expose
     @Column(columnDefinition = "TEXT")
     private String tweet;
-    
+
     @Expose
     @Column(columnDefinition = "TEXT")
     private String tweetTheme;
-    
+
     @Expose
     @Column(columnDefinition = "TEXT")
     private String fbPost;
-    
+
     @Expose
     @Column(columnDefinition = "TEXT")
     private String banners;
-    
+
+    @Expose
+    public Double versionRequired;
+
     // -----------------------------------------------------------------------------------------------//
-    
+
     @Expose
     public Long lastUpdate;
 
@@ -41,74 +44,82 @@ public class Global extends Model {
 
     public static Model.Finder<String, Global> find = new Finder<String, Global>(String.class, Global.class);
 
-   // -----------------------------------------------------------------------------------------------//
-   
-   public static Global current() {
-      try  {
-         return find
-               .where().eq("id", "current")
-               .findUnique();
-      }
-      catch (Exception e) {
-         return null;
-      }
-   }
-   
     // -----------------------------------------------------------------------------------------------//
 
-   public String getId() {
-      return id;
-   }
+    public static Global current() {
+        try  {
+            return find
+                    .where().eq("id", "current")
+                    .findUnique();
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
 
-   public void setId(String id) {
-      this.id = id;
-   }
+    // -----------------------------------------------------------------------------------------------//
 
-   public String getTweet() {
-      return tweet;
-   }
+    public String getId() {
+        return id;
+    }
 
-   public void setTweet(String tweet) {
-      this.tweet = tweet;
-   }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-   public String getTweetTheme() {
-      return tweetTheme;
-   }
+    public String getTweet() {
+        return tweet;
+    }
 
-   public void setTweetTheme(String tweetTheme) {
-      this.tweetTheme = tweetTheme;
-   }
+    public void setTweet(String tweet) {
+        this.tweet = tweet;
+    }
 
-   public String getFbPost() {
-      return fbPost;
-   }
+    public String getTweetTheme() {
+        return tweetTheme;
+    }
 
-   public void setFbPost(String fbPost) {
-       this.fbPost = fbPost;
-   }
+    public void setTweetTheme(String tweetTheme) {
+        this.tweetTheme = tweetTheme;
+    }
 
-   public String getBanners() {
-       return banners;
-   }
+    public String getFbPost() {
+        return fbPost;
+    }
 
-   public void setBanners(String banners) {
-       this.banners = banners;
-   }
+    public void setFbPost(String fbPost) {
+        this.fbPost = fbPost;
+    }
 
-   public Long getLastUpdate() {
-       return lastUpdate;
-   }
+    public String getBanners() {
+        return banners;
+    }
 
-   public void setLastUpdate(Long lastUpdate) {
-       this.lastUpdate = lastUpdate;
-   }
+    public void setBanners(String banners) {
+        this.banners = banners;
+    }
 
-   // -----------------------------------------------------------------------------------------------//
+    public Double getVersionRequired() {
+        return versionRequired;
+    }
 
-   /**
-    * 
-    */
-   private static final long serialVersionUID = 1L;
+    public void setVersionRequired(Double versionRequired) {
+        this.versionRequired = versionRequired;
+    }
+
+    public Long getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Long lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    // -----------------------------------------------------------------------------------------------//
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
 }
