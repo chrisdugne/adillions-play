5d5b2ed53f9f61ee9fa938aafc617d9412236fbe
chrisdugne <chris.dugne@uralys.com>
2014-08-05 16:04:56 +0200

    cors

---
+++ app/controllers/CorsController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import static play.mvc.Controller.request;
+import static play.mvc.Controller.response;
+
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+
+public class CorsController extends Action.Simple {
+
+    //----------------------------------------------------------------------------//
+    
+    public Result call(Http.Context ctx) throws Throwable {
+        Http.Response response = ctx.response();
+        response.setHeader("Access-Control-Allow-Origin", "*");       // Need to add the correct domain in here!!
+        response.setHeader("Access-Control-Allow-Methods", "POST");   // Only allow POST
+        response.setHeader("Access-Control-Max-Age", "300");          // Cache response for 5 minutes
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token");         // Ensure this header is also allowed!  
+        return delegate.call(ctx);
+    }
+    
+    //----------------------------------------------------------------------------//
+
+}
