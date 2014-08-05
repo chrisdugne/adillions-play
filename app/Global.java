f00463098b14dff0594dd9624928d6f1a5fe4f1f
chrisdugne <chris.dugne@uralys.com>
2014-08-05 15:26:02 +0200

    cors

--- app/Global.java
+++ app/Global.java
@@ -3,10 +3,32 @@ import play.mvc.*;
 import play.mvc.Http.RequestHeader;
 
 public class Global extends GlobalSettings {
-     
-    @Override
-    public Result onHandlerNotFound(RequestHeader request) {
-      return controllers.Application.home();
+
+  @Override
+  public Result onHandlerNotFound(RequestHeader request) {
+    return controllers.Application.home();
   }  
-    
+
+  private class ActionWrapper extends Action.Simple {
+    public ActionWrapper(Action<?> action) {
+      this.delegate = action;
+    }
+
+    @Override
+    public Promise<Result> call(Http.Context ctx) throws java.lang.Throwable {
+      Promise<Result> result = this.delegate.call(ctx);
+      Http.Response response = ctx.response();
+      response.setHeader("Access-Control-Allow-Origin", "*");       // Need to add the correct domain in here!!
+      response.setHeader("Access-Control-Allow-Methods", "POST");   // Only allow POST
+      response.setHeader("Access-Control-Max-Age", "300");          // Cache response for 5 minutes
+      response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token");         // Ensure this header is also allowed!  
+      return result;
+    }
+  }
+
+  @Override
+  public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
+    return new ActionWrapper(super.onRequest(request, actionMethod));
+  }
+
 }
