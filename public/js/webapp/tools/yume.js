9498143a12e5da8390ff7ffdd5340cd882fb3f54
chrisdugne <chris.dugne@uralys.com>
2014-09-11 12:39:18 +0200

    yume geolocalisation

--- public/js/webapp/tools/yume.js
+++ public/js/webapp/tools/yume.js
@@ -11,27 +11,29 @@ Yume.init = function() {
     this.yumeInitObj         = new YuMeHTML5SDKInitObject();
 
     //this value needs to be the same as the MedRect div's id
-    this.yumeInitObj.cbMedRectDivId = "cb_medrect1_div";
+    this.yumeInitObj.cbMedRectDivId = 'cb_medrect1_div';
 
     //this value needs to be the same as the MedRect frame's id
-    this.yumeInitObj.cbMedRectIFrameId = "cb_medrect1_frame";
+    this.yumeInitObj.cbMedRectIFrameId = 'cb_medrect1_frame';
 
-    // NOTE: For testing this can be set to "http://shadow01.yumenetworks.com/"
-    this.yumeInitObj.adDomainUrl = "http://plg1.yumenetworks.com/";
+    // NOTE: For testing this can be set to 'http://shadow01.yumenetworks.com/'
+    this.yumeInitObj.adDomainUrl = 'http://plg1.yumenetworks.com/';
 
-    // NOTE: For testing this should be set to "211DaVuJgGj"
-    this.yumeInitObj.domainId = "1747sfOVoyMI";
+    // NOTE: For testing this should be set to '211DaVuJgGj'
+    this.yumeInitObj.domainId = '1747sfOVoyMI';
 
-    this.yumeInitObj.prerollPlaylist = "dynamic_preroll_playlist.vast2xml?imu=medrect";
+    this.yumeInitObj.prerollPlaylist = 'dynamic_preroll_playlist.vast2xml?imu=medrect';
 
     //this value needs to be the same as the HTML5 video element's id on the page
-    this.yumeInitObj.html5VideoId = "player";
+    this.yumeInitObj.html5VideoId = 'player';
 
     //this value needs to be the same as the HTML5 video div element's id on the page
-    this.yumeInitObj.html5VideoDivId = "playerContainer";
+    this.yumeInitObj.html5VideoDivId = 'playerContainer';
 
     //this value needs to be the same as the anchor tag's id around the <video> element
-    this.yumeInitObj.clickTagHRefId = "hRefClickTag";
+    this.yumeInitObj.clickTagHRefId = 'hRefClickTag';
+
+    this.yumeInitObj.bEnableLocationSupport = false;
 
     window.yume_adPlaying = false;
     this.yumeSDKInstance.yume_init(this.yumeInitObj);
@@ -40,54 +42,54 @@ Yume.init = function() {
 window.yumeSDKAdEventListener = function  (yume_event, yume_eventInfo) {
     console.log(yume_event, yume_eventInfo)
     switch (yume_event) {
-        
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_PRESENT:
-            $("#pleaseWait").hide();
+            $('#pleaseWait').hide();
             break;
-            
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_ABSENT:
-            window.location.href = "/yume_novideo";
+            window.location.href = '/yume_novideo';
             break;
-            
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_PLAYING:
-            if(yume_eventInfo == "image_slate")
-                window.location.href = "/yume_wrong_playing";
+            if(yume_eventInfo == 'image_slate')
+                window.location.href = '/yume_wrong_playing';
             else
                 window.yume_adPlaying = true;
             break;
-            
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_COMPLETED:
-            window.location.href = "/yume_completed?status=" + window.yume_adPlaying;
+            window.location.href = '/yume_completed?status=' + window.yume_adPlaying;
             break;
-            
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_ERROR:
-            window.location.href = "/yume_error";
+            window.location.href = '/yume_error';
             break;
-            
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_CLICKED2SITE:
-            console.log("AD_CLICKED2SITE");
-            //console.log("Received AD_CLICKED2SITE event from YuMe HTML5 Plug-in...");
+            console.log('AD_CLICKED2SITE');
+            //console.log('Received AD_CLICKED2SITE event from YuMe HTML5 Plug-in...');
             //do something
             break;
-            
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_CLICKED2vIDEO:
-            console.log("AD_CLICKED2vIDEO");
-            //console.log("Received AD_CLICKED2VIDEO event from YuMe HTML5 Plug-in...");
+            console.log('AD_CLICKED2vIDEO');
+            //console.log('Received AD_CLICKED2VIDEO event from YuMe HTML5 Plug-in...');
             //do something
             break;
 
         case YuMeHTML5SDK.prototype.yume_adEvent.PIP_VIDEO_PLAYING:
-            console.log("PIP_VIDEO_PLAYING");
-            //console.log("Received PIP_VIDEO_PLAYING event from YuMe HTML5 Plug-in...");
+            console.log('PIP_VIDEO_PLAYING');
+            //console.log('Received PIP_VIDEO_PLAYING event from YuMe HTML5 Plug-in...');
             //do something
             break;
-            
+
         case YuMeHTML5SDK.prototype.yume_adEvent.AD_CB_IFRAME:
-            console.log("AD_CB_IFRAME");
-            //console.log("Received AD_CB_IFRAME event from YuMe HTML5 Plug-in..." + yume_eventInfo);
+            console.log('AD_CB_IFRAME');
+            //console.log('Received AD_CB_IFRAME event from YuMe HTML5 Plug-in...' + yume_eventInfo);
             //do something
             break;
-            
+
         default:
             break;
     }
